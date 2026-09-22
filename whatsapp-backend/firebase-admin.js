827d1de0ca24fb7c9ad0f4b1ce84547dd83b3c24
Adebowale Ezekiel <ezekieldev407@gmail.com>
2026-09-22 05:24:14 -0500

    changes!!!!!!!!!

--- whatsapp-backend/firebase-admin.js
+++ whatsapp-backend/firebase-admin.js
@@ -1,17 +1,15 @@
 const admin = require('firebase-admin');
 let serviceAccount;
 if (process.env.FIREBASE_CREDENTIALS) {
-    // Production: Parse from environment variable
     serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 } else {
-    // Local development: Use file
     serviceAccount = require('./serviceAccountKey.json');
 }
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
-// Silently ignore undefined fields instead of throwing
+// Silently ignore undefined fields instead of crashing
 db.settings({ ignoreUndefinedProperties: true });
 const auth = admin.auth();
-module.exports = { admin, db, auth };
+module.exports = { admin, db, auth };
